e8041b0efc27713f22bdba35e0d5a8463fcb1bfb
angelodias <angelossdias@gmail.com>
2020-08-01 12:28:32 -0300

    Implementing simple test to RecyclerView.ViewHolder

--- app/src/androidTest/java/com/example/android/devbyteviewer/ui/DevByteFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/devbyteviewer/ui/DevByteFragmentTest.kt
@@ -1,10 +1,12 @@
 package com.example.android.devbyteviewer.ui
 
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.scrollTo
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.BoundedMatcher
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
@@ -22,17 +24,51 @@ class DevByteFragmentTest {
 
     @Test
     fun scrollToItemToClickTest() {
-        Espresso.onView(withId(R.id.recycler_view))
-                .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
                         3,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        9,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        12,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        17,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        21,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        27,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+        onView(withId(R.id.recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        31,
+                        scrollTo()))
+                .check(matches(isDisplayed()))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<DevByteViewHolder>(
+                        9,
                         click()))
 
-//        onData(inPosition(3))
-//                .inAdapterView(withId(R.id.recycler_view))
+//        onData(inListPosition(3))
+//                .inAdapterView(withId(R.id.list))
 //                .perform(click())
     }
 
-    private fun inPosition(value: Int): Matcher<Any> {
+    private fun inListPosition(value: Int): Matcher<Any> {
         return object : BoundedMatcher<Any, DevByteViewHolder>(DevByteViewHolder::class.java) {
             override fun describeTo(description: Description?) {
                 description?.appendText("has value $value")
